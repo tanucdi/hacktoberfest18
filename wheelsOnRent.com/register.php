8f6585ab7167b420640e645c7849ecbccb1f2917
Ishrak10 <37383284+Ishrak10@users.noreply.github.com>
2018-10-28 19:46:23 +0530

    pr4

---
+++ wheelsOnRent.com/register.php
@@ -0,0 +1,12 @@
+<center><h1>REGISTER</h1>
+    <form action="register2.php" method="post">
+        NAME : <input type="text" name="name" id="name" required placeholder="*" ><BR><BR>
+        AGE : <input type="number" name="age" id="age"  required placeholder="*" ><BR><BR>
+        GENDER : <br>MALE<input type="radio" class="gender" name="gender" id="gender" value="M"  required placeholder="*" >
+        FEMALE<input type="radio" class="gender" name="gender" id="gender" value="F" required placeholder="*" ><BR><BR><BR><BR>
+        ADDRESS:<input type="text" name="address" id="address" size="50"><BR><BR>
+        CITY:<input type="text" name="city" id="city" required placeholder="*" ><BR><BR>
+        <input type="submit" value="NEXT">
+    </form>
+
+</center>
